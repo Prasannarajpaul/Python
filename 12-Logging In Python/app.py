8cd78ad0a58f731ee0ec479122b3f795c138b6e5
Prasannarajpaul <mprasannarajpaul@gmail.com>
2025-02-08 10:00:00 +0530

    Logger

--- 12-Logging In Python/app.py
+++ 12-Logging In Python/app.py
@@ -15,21 +15,28 @@ logger = logging.getLogger("ArithmeticApp")
 
 def add(a,b):
     result = a+b
-    logger.debug(f"Adding {a} and {b} = result")
+    logger.debug(f"Adding {a} and {b} = {result}")
     return result
 def subtract(a,b):
     result = a-b
-    logger.debug(f"Subtracting {b} from {a} = result")
+    logger.debug(f"Subtracting {b} from {a} = {result}")
     return result
 def multiply(a,b):
     result = a*b
-    logger.debug(f"Multiplying {a} and {b} = result")
+    logger.debug(f"Multiplying {a} and {b} = {result}")
     return result
 def divide(a,b):
     try:
         result = a/b
-        logger.debug(f"Dividing {a} by {b} = result")
+        logger.debug(f"Dividing {a} by {b} = {result}")
         return result
     except ZeroDivisionError:
         logger.error(f"Division by Zero Error")
+        return None
 
+
+add(10, 5)
+subtract(15, 10)
+multiply(10, 10)
+divide(10, 0)
+divide(10, 2)
