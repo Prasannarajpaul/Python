ad5ec7ef01dc2aff354299f0879fa5eddfdd4612
Prasannarajpaul <mprasannarajpaul@gmail.com>
2025-02-17 10:00:00 +0530

    Multi Threading and Multi Processing

--- 16-Multithreading and Multiprocessing/advance_multi_processing.py
+++ 16-Multithreading and Multiprocessing/advance_multi_processing.py
@@ -4,7 +4,7 @@ from concurrent.futures import ProcessPoolExecutor
 import time
 
 def square_number(number):
-    time.sleep(1)
+    time.sleep(2)
     return f"Square: {number*number}"
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
---
+++ 16-Multithreading and Multiprocessing/usecase_multi_threading.py
@@ -0,0 +1,8 @@
+'''
+Real-World Example: Multithreading For I/O-bound Tasks
+Scenario: Web Scrapping
+Web Scraping often involves making numerous network requests to
+fetch web pages. These Tasks are I/O-bound because they spend a lot of
+time waiting for responses from servers. Multithreading can significantly
+improve the performance by allowing multiple webpages to be fetched concurrently.
+'''
