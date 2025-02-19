fe640a6ef35024c645ceb5d52762aec2f82aa09b
Prasannarajpaul <mprasannarajpaul@gmail.com>
2025-02-19 22:46:54 +0530

    Multi Threading and Multi Processing

---
+++ 16-Multithreading and Multiprocessing/factorial_multi_processing.py
@@ -0,0 +1,8 @@
+'''
+Real-World Example: Multiprocessing for CPU-bound Tasks
+Scenario: Factorial Calculation
+Factorial calculations, especially for large numbers,
+involve significant computational work, Multiprocessing
+can be used to distribute the workload across multiple
+CPU cores, improving performance.
+'''
