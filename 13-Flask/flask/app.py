69b00c6cd35779ec88a60f1588da33b962a809f7
Prasannarajpaul <mprasannarajpaul@gmail.com>
2025-03-07 23:54:03 +0530

    Flask

--- 13-Flask/flask/app.py
+++ 13-Flask/flask/app.py
@@ -3,13 +3,17 @@ from flask import Flask
 It creates a instance of the Flask class,
 which will be your WSGI (Web Server Gateway Interface) application.
 '''
-
-app=Flask(__name__)
+#### WSGI Application
+app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "Welcome to this Flask course. This should be an amazing course"
+    return "Welcome to this best Flask course. This should be an amazing course"
+
+@app.route("/index")
+def index():
+    return "Welcome to the Index Page"
 
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
