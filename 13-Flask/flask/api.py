2ff5012fdaf7373a630594e0ad31026405ff7d79
Prasannarajpaul <mprasannarajpaul@gmail.com>
2025-03-12 17:55:57 +0530

    Flask

--- 13-Flask/flask/api.py
+++ 13-Flask/flask/api.py
@@ -1,11 +1,11 @@
-### Put and Delete
+### Put and Delete - HTTP Verbs
 ### Working With API's -- JSON
 
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-## Intialize Data in my to do list
+## Intialize Data in my to-do list
 items = [
     {"id": 1, "name": "Item 1", "description": "This is item 1"},
     {"id": 2, "name": "Item 2", "description": "This is item 2"}
@@ -14,6 +14,46 @@ items = [
 @app.route('/')
 def home():
     return "Welcome To The Sample TO-DO List App"
+
+# Get: Retirve all the items
+
+app.route('/items', methods=['GET'])
+def get_items():
+    return jsonify(items)
+
+## Get: Retrieve item based on ID
+app.route('/items/<int:item_id', methods=['GET'])
+def get_item(item_id):
+    item=next((item for item in items if item["id"]==item_id), None)
+    if item is None:
+        return jsonify({"error": "Item is not Found"})
+    return jsonify(item)
+
+## POST: Create a new task
+app.route('/items', methods=['POST'])
+def create_item():
+    if not request.json or not 'name' in request.json:
+        return jsonify({"error":"item not found"})
+    new_item={
+        "id":items[-1]["id"] + 1 if items else 1,
+        "name":request.json['name'],
+        "description":request.josn['description']
+    }
+    items.append(new_item)
+    return jsonify(new_item)
+
+
+
+## PUT: Update an existing item
+@app.route('/items/<int:item_id>', methods=['PUT'])
+def update_item(item_id):
+    item = next((item for item in items if item["id"]==item_id), None)
+    if item is None:
+        return jsonify("error": "Item not found")
+    item['name'] = request.json.get('name', item['name'])
+    
+
+
 
 
 if __name__=='__main__':
